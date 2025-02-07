7c1f9e4200203c21cf99fa015c4129c1aee90d0c
Aayush <aausgh@gmail.com>
2025-02-07 20:49:37 +0545

    something

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,20 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag(string $name): void
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+
+        $this->tags()->attach($tag);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function jobs(): HasMany
-    {
-        return $this->hasMany(Job::class);
     }
 }
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,17 +1,19 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracast</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="text-xl font-bold">Software Developer</h3>
+        <h3 class="text-xl font-bold">{{ $job->title }}</h3>
 
-        <p class="text-sm mt-4">Full Time - $70000 per year</p>
+        <p class="text-sm mt-4">{{ $job->schedule }} - {{ $job->salary }} per year</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag size="small" />
+            @endforeach
         </div>
 
         <x-employer-logo :width="42" />
