84349c862e22bad24ea9d9f05630c37909f2213f
Aayush <aausgh@gmail.com>
2025-02-07 17:10:09 +0545

    tags and latest jobs sections

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,22 +1,19 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">Laracast</div>
 
-    <div class="py-8 font-bold">
-        <h3>Software Developer</h3>
-        <p>Full Time - $70000 per year</p>
+    <div class="py-8">
+        <h3 class="text-xl font-bold">Software Developer</h3>
+
+        <p class="text-sm mt-4">Full Time - $70000 per year</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <a href=""
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href=""
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
-            <a href=""
-                class="bg-white/10 px-2 py-1 rounded-xl text-xs hover:bg-white/25 transition-colors duration-300">Tag</a>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
         </div>
 
-        <img src="https://images.unsplash.com/photo-1537498425277-c283d32ef9db?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGluZm9ybWF0aW9uJTIwdGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D"
-            alt="" class="rounded-xl w-[42px] h-[42px]">
+        <x-employer-logo :width="42" />
     </div>
-</div>
+</x-panel>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-[#060606] text-white font-sans max-w-7xl mx-auto">
+<body class="bg-[#060606] text-white max-w-7xl mx-auto font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
